13b1049feab79f6096854526b089850c0fad0b84
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-03-14 22:13:31 -0300

    adicionados os metodos para remover e alterar

--- src/model/dao/ProdutoDAO.java
+++ src/model/dao/ProdutoDAO.java
@@ -2,6 +2,8 @@ package model.dao;
 
 import java.sql.*;
 import connection.ConnectionFactory;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -11,6 +13,7 @@ public class ProdutoDAO {
 
     Connection conexao = ConnectionFactory.getConnection();
     PreparedStatement stmt = null;
+    ResultSet rs = null;  //guarda o resultado de uma consulta no BD
 
     public void inserir(Produto p) {
         String sql = "INSERT INTO produto (descricao,qtd,preco) VALUES (?,?,?) ";
@@ -25,6 +28,63 @@ public class ProdutoDAO {
             JOptionPane.showMessageDialog(null, "Erro ao salvar: " + ex);
         } finally {
             ConnectionFactory.closeConncetion(conexao, stmt); //fechando as conexões
+        }
+    }
+
+    public List<Produto> listar() {
+        List<Produto> listProdutos = new ArrayList<>();  //criar uma lista de produtos
+        String sql = "SELECT * FROM produto";
+        try {
+            stmt = conexao.prepareStatement(sql);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                Produto p = new Produto();
+                p.setId(rs.getInt("id"));
+                p.setDescricao(rs.getString("descricao"));
+                p.setQtd(rs.getInt("qtd"));
+                p.setPreco(rs.getDouble("preco"));
+
+                listProdutos.add(p);  //adicionar os valores na lista
+
+            }
+        } catch (SQLException ex) {
+            System.err.println("Erro: " + ex);
+
+        } finally {
+            ConnectionFactory.closeConnection(conexao, stmt, rs); //finaliza a conexão
+        }
+
+        return listProdutos;  //retorna a lista de produtos
+    }
+
+    public void atualizar(Produto p) {
+        String sql = "UPDATE produto SET descricao=?,qtd=?,preco=? WHERE id=?";
+        try {
+            stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, p.getDescricao());
+            stmt.setInt(2, p.getQtd());
+            stmt.setDouble(3, p.getPreco());
+            stmt.setInt(4, p.getId());
+            stmt.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Dados alterados com sucesso!");
+        } catch (SQLException ex) {
+            System.err.println("Erro: " + ex);
+        } finally {
+            ConnectionFactory.closeConncetion(conexao, stmt);
+        }
+    }
+
+    public void excluir(Produto p) {
+        String sql = "DELETE FROM produto WHERE id=?";
+        try {
+            stmt = conexao.prepareStatement(sql);
+            stmt.setInt(1, p.getId());
+            stmt.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Dados excluídos com sucesso!");
+        } catch (SQLException ex) {
+            System.err.println("Erro: " + ex);
+        } finally {
+            ConnectionFactory.closeConncetion(conexao, stmt);
         }
 
     }
--- src/view/TelaCadastroUsuario.java
+++ src/view/TelaCadastroUsuario.java
@@ -2,6 +2,9 @@ package view;
 
 import java.sql.*;
 import connection.ConnectionFactory;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
 import model.bean.Produto;
 import model.dao.ProdutoDAO;
 
@@ -15,7 +18,42 @@ public class TelaCadastroUsuario extends javax.swing.JInternalFrame {
 
     public TelaCadastroUsuario() {
         initComponents();
-        conexao = ConnectionFactory.getConnection();
+        conexao = ConnectionFactory.getConnection(); //inicializa a conexão com o BD
+        listaTabela(); //lista o conteúdo da tabela
+        DefaultTableModel modelo = (DefaultTableModel) jtbTabela.getModel();
+        jtbTabela.setRowSorter(new TableRowSorter(modelo));    //ordena os dados da tabela clicando no título
+    }
+
+    //método para ao selecionar uma linha na tabela, os dados são preenchidos nos campos do formulário
+    //necessário adicionar o evento Mouseclicked na tabela
+    private void setarDados() {
+        int setar = jtbTabela.getSelectedRow();
+        txtDesc.setText(jtbTabela.getModel().getValueAt(setar, 1).toString());
+        txtQtd.setText(jtbTabela.getModel().getValueAt(setar, 2).toString());
+        txtPreco.setText(jtbTabela.getModel().getValueAt(setar, 3).toString());
+    }
+
+    public void limparCampos() {
+        txtDesc.setText(null);
+        txtPreco.setText(null);
+        txtQtd.setText(null);
+    }
+
+    public void listaTabela() {
+        DefaultTableModel modelo = (DefaultTableModel) jtbTabela.getModel();
+
+        modelo.setNumRows(0); //para não mostrar duplicados
+
+        ProdutoDAO dao = new ProdutoDAO();
+
+        for (Produto p : dao.listar()) {
+            modelo.addRow(new Object[]{
+                p.getId(),
+                p.getDescricao(),
+                p.getQtd(),
+                p.getPreco()
+            });
+        }
     }
 
     @SuppressWarnings("unchecked")
@@ -31,6 +69,8 @@ public class TelaCadastroUsuario extends javax.swing.JInternalFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jtbTabela = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
 
         setClosable(true);
         setIconifiable(true);
@@ -51,9 +91,14 @@ public class TelaCadastroUsuario extends javax.swing.JInternalFrame {
 
             },
             new String [] {
-                "Descrição", "Quantidade", "Preço"
+                "ID", "Descrição", "Quantidade", "Preço"
             }
         ));
+        jtbTabela.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jtbTabelaMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(jtbTabela);
 
         jButton1.setBackground(new java.awt.Color(0, 153, 255));
@@ -62,6 +107,24 @@ public class TelaCadastroUsuario extends javax.swing.JInternalFrame {
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton4.setBackground(new java.awt.Color(0, 153, 255));
+        jButton4.setForeground(new java.awt.Color(255, 255, 255));
+        jButton4.setText("Atualizar");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
+        jButton5.setBackground(new java.awt.Color(0, 153, 255));
+        jButton5.setForeground(new java.awt.Color(255, 255, 255));
+        jButton5.setText("Excluir");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
             }
         });
 
@@ -87,12 +150,16 @@ public class TelaCadastroUsuario extends javax.swing.JInternalFrame {
                             .addComponent(jLabel3)))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jButton1)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton4)
                         .addGap(0, 0, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(37, Short.MAX_VALUE)
+                .addContainerGap(32, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(jLabel2)
@@ -102,8 +169,11 @@ public class TelaCadastroUsuario extends javax.swing.JInternalFrame {
                     .addComponent(txtDesc, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(txtQtd, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(txtPreco, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(36, 36, 36)
-                .addComponent(jButton1)
+                .addGap(41, 41, 41)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton4)
+                    .addComponent(jButton5))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 208, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
@@ -111,20 +181,77 @@ public class TelaCadastroUsuario extends javax.swing.JInternalFrame {
         setBounds(0, 0, 589, 404);
     }// </editor-fold>//GEN-END:initComponents
 
+    //método para cadastrar ( create )
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
 
-        Produto p = new Produto(); //para alterar os dados em Produto
-        ProdutoDAO dao = new ProdutoDAO(); //para inserir no BD
+        if (txtDesc.getText().isEmpty() || txtPreco.getText().isEmpty() || txtQtd.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Favor preencher todos os campos!");
+        } else {
+            Produto p = new Produto(); //para alterar os dados em Produto
+            ProdutoDAO dao = new ProdutoDAO(); //para inserir no BD
 
-        p.setDescricao(txtDesc.getText());
-        p.setQtd(Integer.parseInt(txtQtd.getText()));  //converter String para Int
-        p.setPreco(Double.parseDouble(txtPreco.getText().replace(",", ".")));  //converter String para Double
+            p.setDescricao(txtDesc.getText());
+            p.setQtd(Integer.parseInt(txtQtd.getText()));  //converter String para Int
+            p.setPreco(Double.parseDouble(txtPreco.getText().replace(",", ".")));  //converter String para Double
 
-        dao.inserir(p); //inserir no BD
+            dao.inserir(p); //inserir no BD
+            listaTabela(); //mostra os dados na tabela
+            limparCampos();
+        }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    //método para alterar ( update)
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+
+        int setar = jtbTabela.getSelectedRow();
+
+        if (txtDesc.getText().isEmpty() || txtPreco.getText().isEmpty() || txtQtd.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Favor preencher todos os campos!");
+        }
+        if (jtbTabela.getSelectedRow() != -1) {  //se a linha estiver selecionada
+            Produto p = new Produto();
+            ProdutoDAO dao = new ProdutoDAO();
+
+            p.setDescricao(txtDesc.getText());
+            p.setQtd(Integer.parseInt(txtQtd.getText()));
+            p.setPreco(Double.parseDouble(txtPreco.getText().replace(",", ".")));
+
+            //pegar o ID a partir da linha selecionada
+            p.setId((int) jtbTabela.getModel().getValueAt(setar, 0)); //convertido objeto para inteiro (int)
+
+            dao.atualizar(p);
+            listaTabela();
+            limparCampos();
+        }
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jtbTabelaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jtbTabelaMouseClicked
+
+        setarDados();
+    }//GEN-LAST:event_jtbTabelaMouseClicked
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+
+        int setar = jtbTabela.getSelectedRow();
+        int confirma = JOptionPane.showConfirmDialog(null, "Confirma a exclusão do produto?", "Atenção", JOptionPane.YES_NO_OPTION);
+        if (confirma == JOptionPane.YES_OPTION) {
+            Produto p = new Produto();
+            ProdutoDAO dao = new ProdutoDAO();
+
+            //pegar o ID a partir da linha selecionada
+            p.setId((int) jtbTabela.getModel().getValueAt(setar, 0)); //convertido objeto para inteiro (int)
+
+            dao.excluir(p); //chama o médoto excluir
+            listaTabela(); //atualiza a tabela após a exclusão
+            limparCampos(); //limpa os campos dos dados excluídos
+        }
+
+    }//GEN-LAST:event_jButton5ActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
